966d37d5cf77f5a171a0231bdf307c5f60d875be
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-08-25 16:51:04 +0530

    create user_management app and extend django user model

---
+++ ecommerce/user_management/admin.py
@@ -0,0 +1,64 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+class UserAdmin(UserAdmin):
+    model = User
+
+    # Define the fields to be used in the user list view and detail view
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "phone_number",
+        "gender",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = ("is_staff", "is_active", "gender")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name", "email", "phone_number", "gender")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "phone_number",
+                    "gender",
+                    "password1",
+                    "password2",
+                    "groups",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
+
+admin.site.register(User, UserAdmin)
---
+++ ecommerce/user_management/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    GENDER_CHOICES = [
+        ("male", "Male"),
+        ("female", "Female"),
+    ]
+
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    gender = models.CharField(
+        max_length=6, choices=GENDER_CHOICES, blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.username
