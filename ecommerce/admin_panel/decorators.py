56a76859d46772233eb57b7ce29f83f3981dde8e
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-08-25 16:56:26 +0530

    modify decorator logic to check user permissions

--- ecommerce/admin_panel/decorators.py
+++ ecommerce/admin_panel/decorators.py
@@ -1,35 +1,37 @@
 from functools import wraps
+
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, JsonResponse
+from django.http import Http404, HttpResponse, HttpResponseForbidden, JsonResponse
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
 
-def check_user_group(group_list=[], type=None):
+def check_user_permission(permission_codename, type=None):
+
     def decorator(func):
         @wraps(func)
         @login_required(login_url="login")
         def inner(request, *args, **kwargs):
             is_check = request.GET.get("is_check", None)
-            user_roles = request.user.groups.values_list("name", flat=True)
+
+            # Check if the user has the required permission
+            has_permission = request.user.has_perm(permission_codename)
+
             if is_check:
-                if request.user.is_superuser or any(
-                    role in user_roles for role in group_list
-                ):
+                if request.user.is_superuser or has_permission:
                     return JsonResponse({"status": "success", "code": 1})
                 else:
                     return JsonResponse({"status": "error", "code": 0})
-
-            if request.user.is_superuser or any(
-                role in user_roles for role in group_list
-            ):
+            if request.user.is_superuser or has_permission:
                 result = func(request, *args, **kwargs)
                 return result
             else:
                 if type == "api":
                     return JsonResponse({"status": "error", "msg": "Permission Denied"})
                 else:
-                    return HttpResponse("HttpResponse Permission Denied")
-
-                # return JsonResponse({"status": "error", "msg": "Permission Denied"})
+                    return render(
+                        request, "admin_panel/404.html"
+                    )  # Raise Http404 for non-API requests
 
         return inner
 
