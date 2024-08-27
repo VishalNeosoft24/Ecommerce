5cd3a82d4eee0c78aaeb5efa72b1d3dc081f0e4e
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-08-27 18:32:58 +0530

    added  functionality to start subprocess to send mail

--- ecommerce/manage.py
+++ ecommerce/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import subprocess
 
 
 def main():
@@ -20,3 +21,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    # Check if the server is starting and then run the process_tasks command
+    if "runserver" in sys.argv:
+        subprocess.Popen(["python", "manage.py", "process_tasks"])
+
+    execute_from_command_line(sys.argv)
