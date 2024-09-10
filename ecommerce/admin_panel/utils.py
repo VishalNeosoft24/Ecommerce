44674ef15237a6f32c722a7bb9d6d5fb49fffd43
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-09-10 17:52:03 +0530

    create a function to send user credentials using mail

--- ecommerce/admin_panel/utils.py
+++ ecommerce/admin_panel/utils.py
@@ -4,11 +4,30 @@ from django.utils.html import strip_tags
 from django.utils import timezone
 from django.template import Template, Context
 
+from django.conf import settings
+
 # Local imports
 from .tasks import send_email_task
 
 
 def send_admin_reply_email(user, user_query, admin_reply, template):
+    """
+    Sends an email to the user with the admin's reply.
+
+    Renders an email template with the provided context, which includes
+    the user, user query, admin reply, and the email template content.
+    The rendered content is then embedded in a base email template.
+
+    Args:
+        user (User): The user to whom the email will be sent.
+        user_query (str): The query or message from the user.
+        admin_reply (str): The reply from the admin.
+        template (EmailTemplate): The email template to use for rendering the message.
+
+    Returns:
+        None: Sends an email using the provided `send_email_task` function.
+    """
+
     # Create the context for rendering the template
     context = {
         "user": user,
@@ -29,7 +48,7 @@ def send_admin_reply_email(user, user_query, admin_reply, template):
         },
     )
     plain_message = strip_tags(html_message)
-    from_email = "vishal.prajapati@neosoftmail.com"
+    from_email = settings.DEFAULT_FROM_EMAIL
     to = user.email
 
     send_email_task(
@@ -37,5 +56,42 @@ def send_admin_reply_email(user, user_query, admin_reply, template):
         plain_message,
         from_email,
         to,
+        html_message=html_message,
+    )
+
+
+def send_user_credentials_email(user, password):
+    """
+    Sends an email to the user with their account credentials.
+
+    Renders an email template with the user's credentials and login URL.
+    The rendered content is used to create the email body, which is sent
+    to the user.
+
+    Args:
+        user (User): The user to whom the email will be sent.
+        password (str): The user's password.
+
+    Returns:
+        None: Sends an email using the provided `send_email_task` function.
+    """
+
+    subject = "Your Account Credentials"
+    login_url = "http://127.0.0.1:8000/admin-panel/login/"
+    context = {
+        "user": user,
+        "password": password,
+        "login_url": login_url,
+    }
+    html_message = render_to_string("admin_panel/user_credentials_email.html", context)
+    plain_message = strip_tags(html_message)
+    from_email = settings.DEFAULT_FROM_EMAIL
+    to_email = user.email
+
+    send_email_task(
+        subject,
+        plain_message,
+        from_email,
+        to_email,
         html_message=html_message,
     )
