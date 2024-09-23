7275bb2a73b6d8d47df7ca3bb4a0a8603f678748
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-09-23 18:32:42 +0530

    Added checkout, place-order and payment urls

--- ecommerce/order_management/urls.py
+++ ecommerce/order_management/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
         views.remove_cart_product,
         name="remove_cart",
     ),
+    path("clear-cart/", views.clear_cart, name="clear_cart"),
     # wishlist
     path("wishlist/", views.wishlist_list, name="wishlist"),
     path(
@@ -25,7 +26,23 @@ urlpatterns = [
         views.remove_wishlist_item,
         name="remove_wishlist_item",
     ),
+    path("clear-wishlist/", views.clear_wishlist, name="clear_wishlist"),
     # Coupon
     path("apply-coupon/", views.apply_coupon, name="apply_coupon"),
     path("remove-coupon/", views.remove_coupon, name="remove_coupon"),
+    # checkout
+    path("checkout/", views.checkout, name="checkout"),
+    # Place Order
+    path("place-order/", views.place_order, name="place_order"),
+    path(
+        "order-successful/<int:order_id>/",
+        views.order_successful,
+        name="order_successful",
+    ),
+    path("add-address", views.add_address, name="add_address"),
+    path("order/<int:order_id>/", views.order_pdf_view, name="order_pdf"),
+    path("order-detail/<int:order_id>/", views.order_detail_view, name="order_detail"),
+    path("orders", views.my_orders, name="my_orders"),
+    # payment
+    path("paymenthandler/", views.paymenthandler, name="paymenthandler"),
 ]
