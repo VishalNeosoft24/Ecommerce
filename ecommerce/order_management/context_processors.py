26885e04bb45d9fca75f0868bf70dd67e53fae4a
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-09-23 18:31:06 +0530

    create a function for wishlist count

--- ecommerce/order_management/context_processors.py
+++ ecommerce/order_management/context_processors.py
@@ -1,4 +1,15 @@
+from order_management.models import UserWishList
+
+
 def cart_item_count(request):
     cart = request.session.get("cart", {})
     item_count = sum(cart.values())
     return {"cart_item_count": item_count}
+
+
+def wishlist_item_count(request):
+    if request.user.is_authenticated:
+        wishlist_item_count = UserWishList.objects.filter(user=request.user).count()
+    else:
+        wishlist_item_count = 0
+    return {"wishlist_item_count": wishlist_item_count}
